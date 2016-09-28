cc42727d46b3fb41655cfcd50e0845da309607a1
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2016-09-28 02:05:20 -0400

    ...

--- barchybrid/src/arc_hybrid.py
+++ barchybrid/src/arc_hybrid.py
@@ -119,7 +119,7 @@ class ArcHybridLSTM:
         topStack = [stack.roots[-i - 1].lstms if len(stack) > i else [self.empty] for i in xrange(self.k)]
         topBuffer = [buf.roots[i].lstms if len(buf) > i else [self.empty] for i in xrange(1)]
         print langVector
-        print type(langVector)
+        print concatenate(langVector)
         input = concatenate(list(chain(*(topStack + topBuffer+langVector))))
 
         if self.hidden2_units > 0:
