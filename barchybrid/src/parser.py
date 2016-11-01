f9cd15ece872ea1d50a7090c93780583e20ad2c6
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2016-11-01 14:51:40 -0400

    dynet mem

--- barchybrid/src/parser.py
+++ barchybrid/src/parser.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     parser.add_option("--userl", action="store_true", dest="rlMostFlag", default=False)
     parser.add_option("--predict", action="store_true", dest="predictFlag", default=False)
     parser.add_option("--partial", action="store_true", dest="partialFlag", default=False)
-    parser.add_option("--dynet-mem", type="int", dest="cnn_mem", default=512)
+    parser.add_option("--dynet_mem", type="int", dest="dynet_mem", default=512)
     parser.add_option("--drop-out", type="float", dest="dropout", default=0)
 
     (options, args) = parser.parse_args()
